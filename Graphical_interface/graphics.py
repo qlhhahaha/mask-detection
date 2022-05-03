ebecd5eed91fb443107902a4d0729e89a5fa1938
xydasixi <1766187138xy@gmail.com>
2022-05-03 20:02:23 +0800

    0

--- Graphical_interface/graphics.py
+++ Graphical_interface/graphics.py
@@ -8,6 +8,13 @@ import torch
 import os
 import shutil
 import cv2
+import threading
+import sys
+from utils.datasets import IMG_FORMATS, VID_FORMATS, LoadImages, LoadStreams
+from utils.general import (LOGGER, check_file, check_img_size, check_imshow, check_requirements, colorstr,
+                           increment_path, non_max_suppression, print_args, scale_coords, strip_optimizer, xyxy2xywh)
+from utils.plots import Annotator, colors, save_one_box
+from utils.torch_utils import select_device, time_sync
 
 class MyMainForm(QMainWindow, Ui_Interface):
     def __init__(self, parent=None):
@@ -48,6 +55,144 @@ class MyMainForm(QMainWindow, Ui_Interface):
         results.save(save_dir='images/temp')
         self.pro_img.setPixmap(QtGui.QPixmap("images/temp/image0.jpg"))
         self.pro_img.setScaledContents(True)
+
+    def vid_upload(self):
+        get_filename_path, ok = QFileDialog.getOpenFileName(self, 'Choose file', '', '*.mp4 *.avi')
+        if ok:
+            self.webcam_detection_btn.setEnabled(False)
+            self.mp4_detection_btn.setEnabled(False)
+            # self.vid_stop_btn.setEnabled(True)
+            self.vid_source = get_filename_path
+            self.webcam = False
+            th = threading.Thread(target=self.detect_vid)
+            th.start()
+
+    def detect_vid(self):
+        # pass
+        model = self.model
+        output_size = self.output_size
+        # source = self.img2predict  # file/dir/URL/glob, 0 for webcam
+        imgsz = [640, 640]  # inference size (pixels)
+        conf_thres = 0.25  # confidence threshold
+        iou_thres = 0.45  # NMS IOU threshold
+        max_det = 1000  # maximum detections per image
+        # device = self.device  # cuda device, i.e. 0 or 0,1,2,3 or cpu
+        view_img = False  # show results
+        save_txt = False  # save results to *.txt
+        save_conf = False  # save confidences in --save-txt labels
+        save_crop = False  # save cropped prediction boxes
+        nosave = False  # do not save images/videos
+        classes = None  # filter by class: --class 0, or --class 0 2 3
+        agnostic_nms = False  # class-agnostic NMS
+        augment = False  # ugmented inference
+        visualize = False  # visualize features
+        line_thickness = 3  # bounding box thickness (pixels)
+        hide_labels = False  # hide labels
+        hide_conf = False  # hide confidences
+        half = False  # use FP16 half-precision inference
+        dnn = False  # use OpenCV DNN for ONNX inference
+        source = str(self.vid_source)
+        webcam = self.webcam
+        device = select_device(self.device)
+        stride, names, pt, jit, onnx = model.stride, model.names, model.pt, model.jit, model.onnx
+        imgsz = check_img_size(imgsz, s=stride)  # check image size
+        save_img = not nosave and not source.endswith('.txt')  # save inference images
+        # Dataloader
+        if webcam:
+            view_img = check_imshow()
+            cudnn.benchmark = True  # set True to speed up constant image size inference
+            dataset = LoadStreams(source, img_size=imgsz, stride=stride, auto=pt and not jit)
+            bs = len(dataset)  # batch_size
+        else:
+            dataset = LoadImages(source, img_size=imgsz, stride=stride, auto=pt and not jit)
+            bs = 1  # batch_size
+        vid_path, vid_writer = [None] * bs, [None] * bs
+        # Run inference
+        if pt and device.type != 'cpu':
+            model(torch.zeros(1, 3, *imgsz).to(device).type_as(next(model.model.parameters())))  # warmup
+        dt, seen = [0.0, 0.0, 0.0], 0
+        for path, im, im0s, vid_cap, s in dataset:
+            t1 = time_sync()
+            im = torch.from_numpy(im).to(device)
+            im = im.half() if half else im.float()  # uint8 to fp16/32
+            im /= 255  # 0 - 255 to 0.0 - 1.0
+            if len(im.shape) == 3:
+                im = im[None]  # expand for batch dim
+            t2 = time_sync()
+            dt[0] += t2 - t1
+            # Inference
+            # visualize = increment_path(save_dir / Path(path).stem, mkdir=True) if visualize else False
+            pred = model(im, augment=augment, visualize=visualize)
+            t3 = time_sync()
+            dt[1] += t3 - t2
+            # NMS
+            pred = non_max_suppression(pred, conf_thres, iou_thres, classes, agnostic_nms, max_det=max_det)
+            dt[2] += time_sync() - t3
+            # Second-stage classifier (optional)
+            # pred = utils.general.apply_classifier(pred, classifier_model, im, im0s)
+            # Process predictions
+            for i, det in enumerate(pred):  # per image
+                seen += 1
+                if webcam:  # batch_size >= 1
+                    p, im0, frame = path[i], im0s[i].copy(), dataset.count
+                    s += f'{i}: '
+                else:
+                    p, im0, frame = path, im0s.copy(), getattr(dataset, 'frame', 0)
+                p = Path(p)  # to Path
+                # save_path = str(save_dir / p.name)  # im.jpg
+                # txt_path = str(save_dir / 'labels' / p.stem) + (
+                #     '' if dataset.mode == 'image' else f'_{frame}')  # im.txt
+                s += '%gx%g ' % im.shape[2:]  # print string
+                gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
+                imc = im0.copy() if save_crop else im0  # for save_crop
+                annotator = Annotator(im0, line_width=line_thickness, example=str(names))
+                if len(det):
+                    # Rescale boxes from img_size to im0 size
+                    det[:, :4] = scale_coords(im.shape[2:], det[:, :4], im0.shape).round()
+
+                    # Print results
+                    for c in det[:, -1].unique():
+                        n = (det[:, -1] == c).sum()  # detections per class
+                        s += f"{n} {names[int(c)]}{'s' * (n > 1)}, "  # add to string
+
+                    # Write results
+                    for *xyxy, conf, cls in reversed(det):
+                        if save_txt:  # Write to file
+                            xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(
+                                -1).tolist()  # normalized xywh
+                            line = (cls, *xywh, conf) if save_conf else (cls, *xywh)  # label format
+                            # with open(txt_path + '.txt', 'a') as f:
+                            #     f.write(('%g ' * len(line)).rstrip() % line + '\n')
+
+                        if save_img or save_crop or view_img:  # Add bbox to image
+                            c = int(cls)  # integer class
+                            label = None if hide_labels else (names[c] if hide_conf else f'{names[c]} {conf:.2f}')
+                            annotator.box_label(xyxy, label, color=colors(c, True))
+                            # if save_crop:
+                            #     save_one_box(xyxy, imc, file=save_dir / 'crops' / names[c] / f'{p.stem}.jpg',
+                            #                  BGR=True)
+                # Print time (inference-only)
+                LOGGER.info(f'{s}Done. ({t3 - t2:.3f}s)')
+                # Stream results
+                # Save results (image with detections)
+                im0 = annotator.result()
+                frame = im0
+                resize_scale = output_size / frame.shape[0]
+                frame_resized = cv2.resize(frame, (0, 0), fx=resize_scale, fy=resize_scale)
+                cv2.imwrite("images/tmp/single_result_vid.jpg", frame_resized)
+                self.vid_img.setPixmap(QPixmap("images/tmp/single_result_vid.jpg"))
+                # self.vid_img
+                # if view_img:
+                # cv2.imshow(str(p), im0)
+                # self.vid_img.setPixmap(QPixmap("images/tmp/single_result_vid.jpg"))
+                # cv2.waitKey(1)  # 1 millisecond
+            if cv2.waitKey(25) & self.stopEvent.is_set() == True:
+                self.stopEvent.clear()
+                self.webcam_detection_btn.setEnabled(True)
+                self.mp4_detection_btn.setEnabled(True)
+                self.reset_vid()
+                break
+        # self.reset_vid()
 
 
 
